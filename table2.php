597217ccdff872480b7fca014dc93bedd7ba6ed7
palmkawee <89444939+palmkawee@users.noreply.github.com>
2021-11-16 09:31:42 +0700

    Update table2.php

--- table2.php
+++ table2.php
@@ -6,9 +6,11 @@
         $row = 100;
         for($i=0;$i<$row;$i++){
           echo "<tr>";
-          echo "<td>คอลัมน์ที่ 1</td>";
-          echo "<td>คอลัมน์ที่ 1</td>";
-        
+          echo "<td>คอลัมน์ที่ 1</td>";       
+          echo "</tr>";
+          
+          echo "<tr>";
+          echo "<td>คอลัมน์ที่ 2</td>";       
           echo "</tr>";
         }
       ?>
