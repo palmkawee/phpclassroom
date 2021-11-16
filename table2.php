6dd1303b9ef942ef8aa2d46ed40d842e7407b082
palmkawee <89444939+palmkawee@users.noreply.github.com>
2021-11-16 09:29:30 +0700

    Update table2.php

--- table2.php
+++ table2.php
@@ -1,12 +1,14 @@
 <html>
   <head></head>
   <body>
-    <table border="6">
+    <table border="1">
       <?php
         $row = 100;
         for($i=0;$i<$row;$i++){
           echo "<tr>";
           echo "<td>คอลัมน์ที่ 1</td>";
+          echo "<td>คอลัมน์ที่ 1</td>";
+        
           echo "</tr>";
         }
       ?>
