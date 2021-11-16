2bc196cdc26f6a087000cb8d6e88993470d78313
palmkawee <89444939+palmkawee@users.noreply.github.com>
2021-11-16 09:44:09 +0700

    Update table2.php

--- table2.php
+++ table2.php
@@ -5,13 +5,17 @@
       <?php
         $row = 100;
         for($i=0;$i<$row;$i++){
-          echo "<tr>";
-              
-              echo "<td>column 1</td>";
+          echo "<tr>";         
+          echo "<td>column 1</td>";
+          echo "<tr>"; 
           echo "<td>column 2</td>";
+          echo "<tr>"; 
           echo "<td>column 3</td>";
+          echo "<tr>"; 
           echo "<td>column 4</td>";
+          echo "<tr>"; 
           echo "<td>column 5</td>";
+          echo "<tr>"; 
           echo "<td>column 6</td>";
           echo "</tr>";
           
